e42a640ce191a82fec43b34afebb6888ba26ab66
Marilise Olimpio <marilisemarinho@gmail.com>
2025-06-10 10:43:20 -0300

    feat: Adiciona pasta com a lista de exercicio 3

---
+++ lista_tres/num_posit_negat.py
@@ -0,0 +1,8 @@
+#Programa que verifica se um número é positivo ou negativo
+
+num = int(input('Informe um número qualquer: '))
+
+if num < 0:
+    print(f'O número {num} é negativo.')
+else:
+    print(f'O número {num} é positivo.')
